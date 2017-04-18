c45c7b8563ae8a0b001693919617f316b6ca7bd5
Joshua Hudson <jhudy11@hotmail.com>
2017-04-17 20:58:07 -0700

    Base shape complete, icons added, assets added

--- Septagon/GameScene.swift
+++ Septagon/GameScene.swift
@@ -10,80 +10,65 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+
+    var colorWheelBase = SKShapeNode()
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let background = SKSpriteNode(imageNamed: "gameBackground")
+        background.size = self.size
+        background.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        background.zPosition = -1
+        self.addChild(background)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        colorWheelBase = SKShapeNode(rectOf: CGSize(width: self.size.width * 0.8, height: self.size.width * 0.8))
+        colorWheelBase.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        colorWheelBase.fillColor = SKColor.clear
+        colorWheelBase.strokeColor = SKColor.clear
+        self.addChild(colorWheelBase)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        prepColorWheel()
+        
+    }
+    
+    func prepColorWheel() {
+        
+        for _ in 0...6 {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+            let side = SKSpriteNode(imageNamed: "side_Blue")
+            let basePosition = CGPoint(x: self.size.width / 2, y: self.size.height / 4)
+            side.position = convert(basePosition, to: colorWheelBase)
+            side.zRotation = -colorWheelBase.zRotation
+            colorWheelBase.addChild(side)
+            
+            // Complete circle of 360 / 7 sides
+            colorWheelBase.zRotation += convertDegreesToRadians(degrees: 360/7)
+            
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Septagon/UtilFuncs.swift
@@ -0,0 +1,16 @@
+//
+//  UtilFuncs.swift
+//  Septagon
+//
+//  Created by Joshua Hudson on 4/17/17.
+//  Copyright © 2017 ParanoidPenguinProductions. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+func convertDegreesToRadians(degrees: CGFloat) -> CGFloat {
+    return degrees * 0.0174533
+}
+
+
